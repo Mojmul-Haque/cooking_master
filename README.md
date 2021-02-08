3e98a6d1ddb464bddd48b1350f76a8eb6b1ac70b
Mojmul Haque <mojmul150@gmail.com>
2021-02-08 23:20:36 +0600

    done something

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# cooking_master
+# live: https://mojmul-haque.github.io/cooking_master/  
--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 let searchQuery = document.getElementById('querySearch');
 const searchBtn = document.getElementById('searhcBtn')
+const recipieDetails = document.getElementById('details')
 const searchResult = document.getElementById('recipe_container')
 
 searchBtn.addEventListener('click', function() {
@@ -9,35 +10,40 @@ searchBtn.addEventListener('click', function() {
     foodApi()
 })
 
-function foodApi() {
+const foodApi = () => {
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchQuery.value}`)
         .then(resp => resp.json())
         .then(data => generateHTML(data.meals))
-        // .then(data => console.log(data))
 }
 
 
-function generateHTML(object) {
+const generateHTML = (object) => {
     console.log(object)
     let htmlCode = '';
 
-    object.map(event => {
+    object.map((event, index) => {
+        let show = index;
         console.log(event)
         const recipeName = event.strMeal
         const recipeImge = event.strMealThumb
 
         htmlCode += `
-                     <li>
+                     <li onclick="myFunction()">
                         <img src="${recipeImge}">
                         <h4>${recipeName}</h4>
                     </li>
         `
         searchResult.innerHTML = htmlCode
 
+
+
+
+
+
     })
 
 
 
-
-
 }
+
+//event handeler
